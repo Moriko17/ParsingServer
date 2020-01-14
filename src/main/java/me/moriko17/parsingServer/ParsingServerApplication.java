85ae04b86ca00df22cbc6cfa1b7ca07ad26a9fc6
Moriko17 <17moriko@gmail.com>
2020-01-14 16:48:57 +0300

    Initial commit

---
+++ src/main/java/me/moriko17/parsingServer/ParsingServerApplication.java
@@ -0,0 +1,13 @@
+package me.moriko17.parsingServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParsingServerApplication.class, args);
+	}
+
+}
